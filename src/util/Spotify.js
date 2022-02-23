154e645bc94d3a0481d49d6781c6220591429f2c
Buff4eyes <claudemokbel@gmail.com>
2022-02-22 17:58:36 -0800

    reset playlist array

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -45,6 +45,30 @@ const Spotify = {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
       window.location = accessUrl
     }
+  },
+  savePlayList(name, trackURIs) {
+    if (!name || !trackURIs.length) {
+      return
+    }
+    
+    const accessToken = Spotify.getAccessToken()
+    const headers = {Authorization: `Bearer ${accessToken}`}
+    let userId;
+
+    return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => response.json()).then(jsonResponse => {
+      userId = jsonResponse.id
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({name: name})
+      }).then(response => response.json()).then(jsonResponse => {const playlistId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({ uris: trackURIs})
+        })
+      })
+    })
   }
 }
 
