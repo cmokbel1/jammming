7798b4a4837aee296a14f468cc28f31187c75bae
Buff4eyes <claudemokbel@gmail.com>
2022-02-22 16:43:35 -0800

    completed Spotify module

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,18 +1,40 @@
-var accessToken 
+var accessToken
 const clientID = '7a8caf6b652d4cbda8eba6dbcd3a9f52'
 const redirectURI = "http://localhost:3000/"
 
 const Spotify = {
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then( response => {
+      return response.json
+    }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artists: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }))
+    })
+  },
+
   getAccessToken() {
     if (accessToken) {
       return accessToken
     }
-      // check for match
-      const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-      const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+    // check for match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
 
     if (accessTokenMatch && expiresInMatch) {
-      accessToken = accessTokenMatch[1] 
+      accessToken = accessTokenMatch[1]
       const expiresIn = Number(expiresInMatch[1])
 
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
